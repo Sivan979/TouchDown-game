63478f3e77ba644673c4767dfc4fdd83866bbf72
sivan <sivanhannan7@gmail.com>
2024-10-23 11:47:14 +0200

    update

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 const gameContent = document.querySelector(".game-content-div");
+
+const contenGenerator = () =>{
+   const gameBlock= document.createElement("div");
+   const frontGameBlock = document.createElement("img");
+   const backGameBlock = document.createElement("div");
+   
+};
