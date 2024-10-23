be48f7b79a70bb62cc06e2d51405a97de8e577a1
sivan <sivanhannan7@gmail.com>
2024-10-23 14:01:09 +0200

    js added html element

--- main.js
+++ main.js
@@ -1,8 +1,24 @@
 const gameContent = document.querySelector(".game-content-div");
 
 const contenGenerator = () =>{
-   const gameBlock= document.createElement("div");
-   const frontGameBlock = document.createElement("img");
-   const backGameBlock = document.createElement("div");
-   
+    for(let i = 0; i < 50; i++) {
+        if(i < 50){
+            const gameBlock= document.createElement("div");
+            const frontGameBlock = document.createElement("img");
+            const backGameBlock = document.createElement("div");
+        
+            gameBlock.classList = "game-block";
+            frontGameBlock.classList = "front-game-block";
+            backGameBlock.classList = "back-game-block";
+        
+            gameContent.appendChild(gameBlock);
+            gameBlock.appendChild(frontGameBlock);
+            gameBlock.appendChild(backGameBlock);
+
+        }
+    };
+    
+
+    
 };
+contenGenerator();
