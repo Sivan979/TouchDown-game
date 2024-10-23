560ef9261156d2daebf9324d50a4004863f711ac
sivan <sivanhannan7@gmail.com>
2024-10-23 19:53:32 +0200

    update

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 const gameContent = document.querySelector(".game-content-div");
+const images = [ 
+    {name: "helmet", imgsrc:"./images/helmet.png"},
+    {name: "ball", imgsrc:"./images/ball.avif"}
+];
 
 const contenGenerator = () =>{
+    const rows = []; // Will store rows of blocks
+
     for(let i = 0; i < 50; i++) {
         if(i < 50){
             const gameBlock= document.createElement("div");
@@ -15,10 +21,11 @@ const contenGenerator = () =>{
             gameBlock.appendChild(frontGameBlock);
             gameBlock.appendChild(backGameBlock);
 
+            
+
+
         }
     };
-    
-
-    
 };
+
 contenGenerator();
