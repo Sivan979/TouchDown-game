0f2292e23ef8d0852aeb6f019d7d984eff2e0010
sivan <sivanhannan7@gmail.com>
2024-11-03 15:53:24 +0100

    small field updated

--- main.js
+++ main.js
@@ -15,6 +15,8 @@ const images = [
     {name: "catched-ball", imgsrc: "./images/catched-ball.png"}
 ];
 const rowMultipliers = [1.20, 1.50, 1.90, 2.40, 3.10, 4.50, 5.80, 6.90, 8.20, 10.00];
+const mediumRowMultipliers = [1.30, 1.70, 2.30, 3.00, 4.00, 5.30, 7.10, 9.50];
+const smallRowMultipliers = [1.40, 2.10, 3.20, 4.80, 7.20];
 const betAmounts = [0.20, 0.40, 0.60, 1.00, 1.50, 2.00, 3.00, 4.00, 5.00, 7.00, 10.00, 15.00, 20.00, 30.00, 40.00, 50.00, 70.00, 100.00];
 
 let gameStarted = false;
@@ -162,6 +164,20 @@ playbtn.addEventListener("click", function () {
         // Clear the game area and regenerate content
         gameContent.innerHTML= "";
         mediumBtnF();
+    } else if(smallFieldCreated === "true"){
+        gameStarted = true;
+        gameOver = false;
+        activeRowIndexS = 5;
+        currentMultiplier = 0;
+        balance = balance - betAmounts[currentBetIndex];
+        updateBalance();
+        updateProfit(currentMultiplier);
+        playbtn.disabled = true;
+        minusBtn.disabled = true;
+        plusBtn.disabled = true;
+        // Clear the game area and regenerate content
+        gameContent.innerHTML= "";
+        smallBtnF();
     }
     
 });
@@ -328,6 +344,61 @@ function handleBlockClick(frontGameBlock, rowDiv, rowIndex) {
     if(mediumFieldCreated === "true"){
         mediumFieldRules();
     }
+    if(smallFieldCreated === "true"){
+        smallFieldRules();
+    }
+
+    function smallFieldRules(){
+        if (!gameStarted || gameOver) {
+            return; // Ignore clicks if the game hasn't started or is over
+        }
+        if (rowIndex !== activeRowIndexS) {
+            return;
+        }
+        if (rowDiv.getAttribute("data-ball-placed") === "true") {
+            return;
+        }
+
+        if (frontGameBlock.src.includes('helmet')) {
+            frontGameBlock.src = './images/catched-ball.png';
+            rowDiv.setAttribute("data-ball-placed", "true");
+
+            gameOver = true;
+            gameStarted = false;
+            playbtn.disabled = false;
+            minusBtn.disabled = false;
+            plusBtn.disabled = false;
+            cashOutBtn.disabled = true;
+
+            return;
+        } else {
+            frontGameBlock.src = './images/ball.avif';
+            rowDiv.setAttribute("data-ball-placed", "true");
+            cashOutBtn.disabled = false;
+            
+        }
+        const currentRewardIndex = 5 - activeRowIndexS;
+        if (currentRewardIndex <= mediumRowMultipliers.length) {
+            profits = betAmounts[currentBetIndex] * smallRowMultipliers[currentRewardIndex];
+            updateProfit(profits);
+        }
+
+        // Move to the next row above
+        if (activeRowIndexS > 1) {
+            activeRowIndexS--;
+        }else {
+            // Player has successfully reached the top row (won the game)
+            gameOver = true;
+            gameStarted = false;
+            playbtn.disabled = false;
+            minusBtn.disabled = false;
+            plusBtn.disabled = false;
+            balance = balance + profits;
+            cashOutBtn.disabled = true;
+            updateBalance();
+            return;
+        }
+    }
 
     function mediumFieldRules(){
         if (!gameStarted || gameOver) {
@@ -359,8 +430,8 @@ function handleBlockClick(frontGameBlock, rowDiv, rowIndex) {
             
         }
         const currentRewardIndex = 8 - activeRowIndexM;
-        if (currentRewardIndex <= rowMultipliers.length) {
-            profits = betAmounts[currentBetIndex] * rowMultipliers[currentRewardIndex];
+        if (currentRewardIndex <= mediumRowMultipliers.length) {
+            profits = betAmounts[currentBetIndex] * mediumRowMultipliers[currentRewardIndex];
             updateProfit(profits);
         }
 
